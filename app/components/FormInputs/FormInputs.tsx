060849c1f44688e4d89d79b002d0b46efdf6e282
Santiago Iannello <89161674+SantiSTC@users.noreply.github.com>
2024-02-29 19:31:43 -0300

    Update FormInputs.tsx

--- app/components/FormInputs/FormInputs.tsx
+++ app/components/FormInputs/FormInputs.tsx
@@ -20,7 +20,7 @@ function FormInputs({
         id={id}
         name={name}
         placeholder={placeholder}
-        className={`mt-1 h-12 w-72 rounded-sm text-sm border-none bg-gray-800 p-2 text-white focus:placeholder-transparent focus:outline-none`}
+        className={`mt-1 h-12 w-72 rounded-md text-sm border-none bg-gray-800 p-2 text-white shadow-md focus:placeholder-transparent focus:outline-none`}
       />
     </div>
   );
