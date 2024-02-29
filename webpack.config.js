4c613061d3f7b80cbd20e75846d41a3ac97f1a04
Santiago Iannello <santiagoiannellot@gmail.com>
2024-02-29 17:29:38 -0300

    english language added

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
+  };
+  
