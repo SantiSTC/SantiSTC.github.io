7c2a871e8291a56fb71b9ecc28a565c6159331b3
Santiago Iannello <santiagoiannellot@gmail.com>
2024-02-29 17:41:34 -0300

    english language added

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,5 +1,5 @@
-"use client"
-import React, { useEffect, useRef, useState } from 'react';
+
+import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useTranslation } from 'react-i18next';
